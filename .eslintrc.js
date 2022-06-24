79566e2dd219331b8d778cf656e0fa82d2f5a58a
yangxueqian <yangxueqian@huobitech.com>
2022-06-24 17:05:27 +0800

    fix: config vue3 eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/vue3-strongly-recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint'
+  ],
+  // 自定义规则
+  rules: {
+  }
+}
