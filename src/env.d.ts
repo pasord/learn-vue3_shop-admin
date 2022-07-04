dd5a2e6d3e37101efbc13ab790e5a4e98387813a
pasord <pasord@qq.com>
2022-07-04 23:19:10 +0800

    feat: 环境变量和模式

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,17 @@
 // env.d.ts 是声明的全局类型
 
-// 👇🏻三斜线引入类型声明文件，只是为了放到 env.d.ts 一起作为全局类型
+/**
+ * 三斜线引入类型声明文件，放到 env.d.ts 里, 只是为了一起作为全局类型
+ * Vite 在 vite/client.d.ts 中为 import.meta.env 提供了类型定义
+ */
 /// <reference types="vite/client" />
+
+// 补充import.meta.env的类型，有报错先disable了
+// eslint-disable-next-line no-unused-vars
+interface ImportMetaEnv {
+  readonly VITE_API_BASEURL: string
+  // 更多环境变量...
+}
 
 // 👇🏻扩展原有模块的类型：给所有的.vue文件都默认导出一个为component类型，
 declare module '*.vue' {
