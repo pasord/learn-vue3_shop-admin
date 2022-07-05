8838d15fe201dd3e82507d2f729dd5f27777e436
pasord <pasord@qq.com>
2022-07-05 22:50:54 +0800

    feat: layout页面布局 路由layout

---
+++ src/layout/AppLayout.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-container>
+    <el-aside width="200px">
+      <AppMenu />
+    </el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>
+        <!-- 子路由出口 -->
+        <router-view />
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script setup lang="ts">
+import AppMenu from './components/AppMenu.vue'
+</script>
+
+<style lang="scss" scoped>
+// el样式可直接生效
+.el-container {
+    height: 100vh;
+}
+.el-header, .el-footer {
+    background-color: #b3c0d1;
+}
+.el-aside {
+    background-color: #304156;
+}
+.el-main {
+    background-color: #e9eef3;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,19 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import AppLayout from '@/layout/AppLayout.vue'
 
 // 路由规则及类型
 const routes:RouteRecordRaw[] = [
   {
     path: '/',
     name: 'home',
-    component: () => import('@/views/home/index.vue')
+    component: AppLayout, // 这里配置layout还是不错的
+    children: [
+      {
+        path: '', // 默认子路由，父级路由name可不加
+        name: 'home',
+        component: () => import('@/views/home/index.vue')
+      }
+    ]
   },
   {
     path: '/demo',
