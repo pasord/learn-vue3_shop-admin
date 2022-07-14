91ae8acf898b03edf27661cba9e2bf4c6ded24be
pasord <pasord@qq.com>
2022-07-14 09:20:59 +0800

    feat: request 响应拦截加统一处理接口错误

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosRequestConfig } from 'axios'
+import { ElMessage } from 'element-plus'
 
 const request = axios.create({
   baseURL: import.meta.env.VITE_API_BASEURL
@@ -7,7 +8,6 @@ const request = axios.create({
 /** note
  * request 不支持泛型，无法返回类型数据
  * 但 request.get、post、put 等支持响应数据泛型
- *
  */
 
 // 添加请求拦截器
@@ -21,7 +21,12 @@ request.interceptors.request.use(function (config) {
 
 // 添加响应拦截器
 request.interceptors.response.use(function (response) {
-  // 对响应数据做点什么
+  // 统一处理接口响应错误，如 token 过期无效、服务端业务异常等
+  if (response.data.statsu && response.data.statsu !== 200) {
+    ElMessage.error(response.data.msg || '请求失败，请重试')
+    return Promise.reject(response.data) // 其实
+  }
+
   return response
 }, function (error) {
   // 对响应错误做点什么
@@ -33,6 +38,6 @@ request.interceptors.response.use(function (response) {
 // 返回的data直接断言为传入的类型T
 export default <T = any>(config: AxiosRequestConfig<any>) => {
   return request(config).then((res) => {
-    return res.data.data as T // 直接把最终的接口data断言为T
+    return (res.data.data || res.data) as T // 直接把最终的接口data断言为T
   })
 }
